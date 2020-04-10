b69bee449de449571e95374ff333269d2240352e
SaucyPigeon <emilianb@hotmail.co.uk>
2020-04-10 18:26:10 +0100

    Added loader class, added about.xml

---
+++ Source/ResourceReadout/ResourceReadout-RW1.1/Loader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Reflection;
+
+namespace ResourceReadout
+{
+	[StaticConstructorOnStartup]
+	public static class Loader
+	{
+		static Loader()
+		{
+			const string Id = "com.saucypigeon.rimworld.mod.resourcereadout";
+#if DEBUG
+			Harmony.DEBUG = true;
+			Log.Warning($"{Id} is in debug mode. Please contact the mod author if you see this.");
+#endif
+			var harmony = new Harmony(Id);
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
---
+++ Source/ResourceReadout/ResourceReadout-RW1.1/Patches/Verse_TreeNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace ResourceReadout.Patches
+{
+#if DEBUG
+	[HarmonyPatch(typeof(TreeNode))]
+	public static class Verse_TreeNode
+	{
+		[HarmonyPatch(nameof(TreeNode.SetOpen))]
+		[HarmonyPostfix]
+		public static void Postfix(int mask, bool val, int __openBits)
+		{
+			Log.Message("TreeNode.SetOpen_Postfix (start)");
+			Log.Message($"Value of mask parameter: {mask}");
+			Log.Message($"Value of val parameter: {val}");
+			Log.Message($"Value of openBits field: {__openBits}");
+			Log.Message("TreeNode.SetOpen_Postfix (end)");
+		}
+	}
+#endif
+}
